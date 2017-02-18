cd37f715c7777ff735f185a724f8570cb9ca9ab7
Tatsuya <tatsuya.kaiden@gmail.com>
2017-02-18 13:39:24 +0900

    Delete comment

--- trans-id.rb
+++ trans-id.rb
@@ -6,13 +6,6 @@ def trans(pre)
   File.open("translations/id/#{filename}", "w") do |dest|
     source.each_line do |line|
       dest.puts line =~ /\s{2}-\sname:\s/ ? "#{$&}#{pre ? "pokemon-" : ""}#{"%04d" % id += 1}" : line
-      # if m = (line.match /\s{2}-\sname:\s/)
-      # if line =~ /\s{2}-\sname:\s/
-      #   puts $&
-      #   # puts "#{$1}#{pre ? "prefix-" : ""}#{++id}"
-      # else
-      #   puts line
-      # end
     end
   end
   end
